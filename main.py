5c0ca9bc606118bf9ce62e72da03b70c0b326e39
corkyrapier <worldEGORa@gmail.com>
2023-02-23 12:23:14 +0300

    sub and unsub

--- main.py
+++ main.py
@@ -33,11 +33,13 @@ class addAnnonceState(StatesGroup):
 @dp.callback_query_handler(text_contains='restart')
 async def start_hendler(message: types.Message or types.CallbackQuery):
     if 'text' not in message:
+        await bot.delete_message(message.message.chat.id, message.message.message_id)
         last = Annonce.get_last_annonce()[0]
         text_post_in_channel = f"""
         Имя: <b>{last[1]}</b>
         Описание:: {last[2]}
         Дата начала мероприятия: {last[3]} {last[4]}
+        <i>Код: {last[7]}</i>
         """
         subscribe = types.inline_keyboard.InlineKeyboardButton(text="Подписаться", callback_data="subscribe")
         chat_kb = types.InlineKeyboardMarkup()
@@ -64,13 +66,22 @@ async def start_hendler(message: types.Message or types.CallbackQuery):
 
 @dp.callback_query_handler(text_contains='subscribe')
 async def subscribe_on_annonce(query: types.CallbackQuery, state: FSMContext):
-    unsubscribe = types.inline_keyboard.InlineKeyboardButton(text="Отписаться", callback_data="----")
+    await bot.forward_message(query['from'].id, query.message.chat.id, query.message.message_id)
+    code_u = query.message.text.split(':')[-1].strip()
+    response = Subscribe.add_sub([code_u, query['from'].id])
+    unsub = types.inline_keyboard.InlineKeyboardButton(text="Отписаться", callback_data="delete_sub")
     unsub_kb = types.InlineKeyboardMarkup()
-    unsub_kb.add(unsubscribe)
-    await query.message.edit_reply_markup(reply_markup=unsub_kb)
-    # print(query)
-    # await bot.edit_message_reply_markup(query['from'].id, query.message.message_id, reply_markup=unsub_kb)
-    # await query.message.edit_text()
+    unsub_kb.add(unsub)
+    if response:
+        await bot.send_message(query['from'].id, f'Вы подписаны на событие. (Код: {str(code_u)})', reply_markup=unsub_kb)
+    else:
+        await bot.send_message(query['from'].id, f'Невозможно подписаться, вы уже подписаны на это событие. (Код: {str(code_u)})', reply_markup=unsub_kb)
+
+@dp.callback_query_handler(text_contains='delete_sub')
+async def delete_sub_annonce(query: types.CallbackQuery, state: FSMContext):
+    code_u = query.message.text.split(':')[-1].strip().replace(')', '')
+    Subscribe.delete_sub([code_u, query['from'].id])
+    await query.message.edit_text('Вы отписаны от мероприятия.')
 
 @dp.callback_query_handler(text_contains='add_new_annonce')
 async def add_new_annonce(query: types.CallbackQuery, state: FSMContext):
@@ -83,21 +94,6 @@ async def process_name(message: types.Message, state: FSMContext):
         data['name'] = message.text
 
     await addAnnonceState.next()
-#     genre_list = Genre.get_list()
-#     keyboard = types.InlineKeyboardMarkup()
-#     for genre in genre_list:
-#         button = types.inline_keyboard.InlineKeyboardButton(text=str(genre[1]), callback_data=str(genre[0]))
-#         keyboard.add(button)
-#     await message.reply('Выберите жанр мероприятия:', reply_markup=keyboard)
-
-# # @dp.message_handler(state=addAnnonceState.genre_id)
-# @dp.callback_query_handler(state=addAnnonceState.genre_id)
-# async def process_genre(query: types.CallbackQuery, state: FSMContext):
-#     print(query)
-#     async with state.proxy() as data:
-#         data['genre_id'] = query.data
-
-#     await addAnnonceState.next()
     await message.answer('Введите описание мероприятия:')
 
 @dp.message_handler(state=addAnnonceState.discription)
@@ -106,7 +102,7 @@ async def process_discription(message: types.Message, state: FSMContext):
         data['discription'] = message.text
 
     await addAnnonceState.next()
-    await message.reply('Введите дату начала в формате - день.месяц.год (например - 22.12.2023):')
+    await message.answer('Введите дату начала в формате - день.месяц.год (например - 22.12.2023):')
 
 @dp.message_handler(state=addAnnonceState.start_date)
 async def process_start_date(message: types.Message, state: FSMContext):
@@ -114,20 +110,18 @@ async def process_start_date(message: types.Message, state: FSMContext):
         data['start_date'] = message.text
 
     await addAnnonceState.next()
-    await message.reply('Введите время начала (например - 18:00):')
+    await message.answer('Введите время начала (например - 18:00):')
 
 @dp.message_handler(state=addAnnonceState.start_time)
 async def process_start_time(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['start_time'] = message.text
-        print(data['name'], data['discription'], data['start_date'], data['start_time'])
         await message.answer("Добавлено новое мероприятие!")
         await message.answer(
                             f"Имя: {data['name']}\n"
                             f"Описание:: {data['discription']}\n"
                             f"Дата начала мероприятия: {data['start_date']} {data['start_time']}\n")
         check = Annonce.check_double([message.from_user.id, data['start_date'], data['start_time']])
-        print(check)
         if check != []:
             Annonce.delete(check[0])
         add_data = [
@@ -147,50 +141,6 @@ async def process_start_time(message: types.Message, state: FSMContext):
     await message.answer('Данные верны?', reply_markup=keyboard)
     await state.finish()
 
-# dp.message_handler(message='hello')
-# async def add_annonse_db():
-#     async with state.proxy() as data:
-#         print('Добавил в базу') #не забыть поставить user_id для какждой новости
-#     add_annonce = types.inline_keyboard.InlineKeyboardButton(text="Новый анонс", callback_data="add_new_annonce")
-#     annonce_lsit = types.inline_keyboard.InlineKeyboardButton(text="Посмотреть анонсы", callback_data='кал')
-#     keyboard = types.InlineKeyboardMarkup()
-#     keyboard.add(add_annonce, annonce_lsit)
-#     await message.answer("Вы хотите посмотреть анонсы мероприятий или анонсировать новое?", reply_markup=keyboard)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-
-
-
-
-
-
-# {
-#     "message_id": 438,
-#     "from": {"id": 826586914, "is_bot": false, "first_name": "Егор",
-#             "last_name": "Мащенко", "username": "CorkyRapier", "language_code": "ru"},
-#     "chat": {"id": 826586914, "first_name": "Егор", "last_name": "Мащенко", "username": "CorkyRapier", "type": "private"},
-#     "date": 1676822389,
-#     "text": "/start",
-#     "entities": [{"type": "bot_command", "offset": 0, "length": 6}]}
-
-# {
-#     "id": "3550163766322039316",
-#     "from": {
-#         "id": 826586914, "is_bot": false, "first_name": "Егор", "last_name":
-#         "Мащенко", "username": "CorkyRapier", "language_code": "ru"},
-#     "message": {
-#         "message_id": 450, "from": {"id": 6297688500, "is_bot": true,
-#         "first_name": "Пространство роста и расширения. Бот.", "username": "space_for_growth_bot"},
-#     "chat": {
-#         "id": 826586914, "first_name": "Егор", "last_name": "Мащенко",
-#         "username": "CorkyRapier", "type": "private"
-#         },
-#     "date": 1676822395,
-#     "text": "Данные верны?",
-#     "reply_markup": {
-#         "inline_keyboard": [[{"text": "Да", "callback_data": "restart"},
-#         {"text": "Нет", "callback_data": "add_new_annonce"}]]}},
-#     "chat_instance": "4830946017084993614", "data": "restart"
-#     }
--- models/subscribe_annonce.py
+++ models/subscribe_annonce.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 import time
 
 from connect import con, cur
@@ -6,5 +7,22 @@ from connect import con, cur
 class Subscribe:
     def add_sub(data):
         with con:
-            result = cur.execute("INSERT INTO subscribe_annonce(sub_id, annonce_id, tg_id_user) VALUES(?, ?, ?)", data)
-            return result
+            annonce_on_cod = cur.execute("SELECT annonce_id FROM annonce WHERE cod_u = ?", data[0]).fetchall()
+            check_sub = cur.execute("""SELECT sub_id FROM subscribe_annonce 
+                                        WHERE annonce_id = ? AND tg_id_user = ?""", [annonce_on_cod[0][0], data[1]]).fetchall()
+            if check_sub == []:
+                data = [str(uuid.uuid4()), annonce_on_cod[0][0], data[1]]
+                result = cur.execute("""INSERT INTO subscribe_annonce(sub_id, annonce_id, tg_id_user) 
+                                    VALUES(?, ?, ?)""", data)
+                con.commit()
+                return True
+            return False
+
+    def delete_sub(data):
+        with con:
+            annonce_on_cod = cur.execute("SELECT annonce_id FROM annonce WHERE cod_u = ?", data[0]).fetchall()
+            sub_id = cur.execute("""SELECT sub_id FROM subscribe_annonce 
+                                        WHERE annonce_id = ? AND tg_id_user = ?""", [annonce_on_cod[0][0], data[1]]).fetchall()
+            cur.execute('DELETE FROM subscribe_annonce WHERE sub_id = ?', sub_id[0])
+            con.commit()
+
